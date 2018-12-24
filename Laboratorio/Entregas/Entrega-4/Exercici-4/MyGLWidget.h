e0bce7ac42bf6346ad72113db897dcf4808a1ccb
Marian Dumitru Danci <34145543+dumitrux@users.noreply.github.com>
2018-12-24 19:27:05 +0100

    Entrega-4

---
+++ Laboratorio/Entregas/Entrega-4/Exercici-4/MyGLWidget.h
@@ -0,0 +1,77 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "model.h"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core 
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    virtual void keyPressEvent (QKeyEvent *event);
+    // mouse{Press/Release/Move}Event - Són cridades quan es realitza l'event 
+    // corresponent de ratolí
+    virtual void mousePressEvent (QMouseEvent *event);
+    virtual void mouseReleaseEvent (QMouseEvent *event);
+    virtual void mouseMoveEvent (QMouseEvent *event);
+
+  private:
+    void createBuffersModel ();
+    void createBuffersTerraIParet ();
+    void carregaShaders ();
+    void iniEscena ();
+    void iniCamera ();
+    void projectTransform ();
+    void viewTransform ();
+    void modelTransformTerra ();
+    void modelTransformModel ();
+    void modelTransformModel2 ();
+    void calculaCapsaModel ();
+    void calculSCA ();
+
+    // VAO names
+    GLuint VAO_Patr;
+    GLuint VAO_Terra;
+    // Program
+    QOpenGLShaderProgram *program;
+    // uniform locations
+    GLuint transLoc, projLoc, viewLoc;
+    // attribute locations
+    GLuint vertexLoc, normalLoc, matambLoc, matdiffLoc, matspecLoc, matshinLoc;
+    GLuint posFocusLoc;
+
+    // model
+    Model patr;
+    // paràmetres calculats a partir de la capsa contenidora del model
+    glm::vec3 centreBasePatr, posFocus1, posFocus2, mov1, mov2;
+    float escala, escala2;
+    // radi de l'escena
+    float radiEsc, raw;
+    glm::mat4 View;  // Matriu de posició i orientació
+
+    typedef  enum {NONE, ROTATE} InteractiveAction;
+    InteractiveAction DoingInteractive;
+    int xClick, yClick, estat;
+    float angleY;
+    bool perspectiva;
+};
+
---
+++ Laboratorio/Entregas/Entrega-4/Exercici-4/shaders/fragshad.frag
@@ -0,0 +1,68 @@
+#version 330 core
+
+in vec3 fcolor;
+
+in vec3 vertexF;
+in vec3 normalF;
+
+in vec3 matambF;
+in vec3 matdiffF;
+in vec3 matspecF;
+in float matshinF;
+
+uniform vec3 posFocus;
+
+
+out vec4 FragColor;
+
+// Valors per als components que necessitem dels focus de llum
+vec3 colFocus = vec3(0.8, 0.8, 0.8);
+vec3 llumAmbient = vec3(0.2, 0.2, 0.2);
+//ec3 posFocus = vec3(1, 1, 1);  // en SCA
+
+vec3 Lambert (vec3 NormSCO, vec3 L) 
+{
+    // S'assumeix que els vectors que es reben com a paràmetres estan normalitzats
+
+    // Inicialitzem color a component ambient
+    vec3 colRes = llumAmbient * matambF;
+
+    // Afegim component difusa, si n'hi ha
+    if (dot (L, NormSCO) > 0)
+      colRes = colRes + colFocus * matdiffF * dot (L, NormSCO);
+    return (colRes);
+}
+
+vec3 Phong (vec3 NormSCO, vec3 L, vec4 vertSCO) 
+{
+    // Els vectors estan normalitzats
+
+    // Inicialitzem color a Lambert
+    vec3 colRes = Lambert (NormSCO, L);
+
+    // Calculem R i V
+    if (dot(NormSCO,L) < 0)
+      return colRes;  // no hi ha component especular
+
+    vec3 R = reflect(-L, NormSCO); // equival a: normalize (2.0*dot(NormSCO,L)*NormSCO - L);
+    vec3 V = normalize(-vertSCO.xyz);
+
+    if ((dot(R, V) < 0) || (matshinF == 0))
+      return colRes;  // no hi ha component especular
+    
+    // Afegim la component especular
+    float shine = pow(max(0.0, dot(R, V)), matshinF);
+    return (colRes + matspecF * colFocus * shine); 
+}
+
+
+void main()
+{	
+  // Dixar posicio del focus de llum en SCA
+  vec3 L = normalize(posFocus - vertexF.xyz); // Direccio llum
+  vec3 NM = normalize(normalF); 
+  
+  vec3 color = Phong(NM, L, vec4(vertexF, 1));
+  FragColor = vec4(color, 1);
+  //FragColor = vec4(fcolor,1);
+}
